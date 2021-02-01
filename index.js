4331c37835eb13de1f2ffe7b66f190520c7e4d55
rohitabraham02 <rohit.abraham02@gmail.com>
2021-02-01 18:14:21 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  const name = process.env.NAME || 'World test';
+  const name = process.env.NAME || 'World version1';
   res.send(`Hello ${name}!`);
 });
 
